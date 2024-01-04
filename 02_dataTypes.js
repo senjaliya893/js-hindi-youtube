b9654b0fcd43de2b5b890ce3231ad7cea88d6691
ViVEK SENJALiYA <154584056+senjaliya893@users.noreply.github.com>
2024-01-04 16:26:00 +0530

    dataType

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+console.log("vivek")
+
+
+let name = "vivek"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
